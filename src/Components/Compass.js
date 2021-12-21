282023be711979bc6fd6bcb83e071fc47645ca92
Rudolf.Krizan <rudolf.krizan@kyndryl.com>
2021-12-21 15:45:57 +0100

    arrow_working

--- src/Components/Compass.js
+++ src/Components/Compass.js
@@ -4,7 +4,43 @@ import React, { useState } from "react";
 function Compass(){
     let compass;
     let isIOS = /iPad|iPhone|iPod/.test(navigator.platform)
-|| (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1)
+    || (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1)
+    let pointDegree;
+
+    function init() {
+      requestPer()
+      navigator.geolocation.getCurrentPosition(locationHandler);
+    }
+
+    function locationHandler(position) {
+      const { latitude, longitude } = position.coords;
+      pointDegree = calcDegreeToPoint(latitude, longitude);
+
+      if (pointDegree < 0) {
+        pointDegree = pointDegree + 360;
+      }
+    }
+
+    function calcDegreeToPoint(latitude, longitude) {
+      // Qibla geolocation
+      const point = {
+        lat: 48.505034,
+        lng: 17.428396,
+      };
+
+      const phiK = (point.lat * Math.PI) / 180.0;
+      const lambdaK = (point.lng * Math.PI) / 180.0;
+      const phi = (latitude * Math.PI) / 180.0;
+      const lambda = (longitude * Math.PI) / 180.0;
+      const psi =
+        (180.0 / Math.PI) *
+        Math.atan2(
+          Math.sin(lambdaK - lambda),
+          Math.cos(phi) * Math.tan(phiK) -
+            Math.sin(phi) * Math.cos(lambdaK - lambda)
+        );
+      return Math.round(psi);
+    }
 
     function requestPer(){
     if (isIOS) {
@@ -18,17 +54,31 @@ function Compass(){
         })
         .catch(() => alert("not supported"));
     } else {
-    alert("android")
     window.addEventListener("deviceorientationabsolute", handler, true);
     }
   }
     function handler(e) {
-        compass = e.webkitCompassHeading || Math.abs(e.alpha - 360);
-        setTransform(`translate(-50%, -50%) rotate(${-compass}deg)`)
+      compass = e.webkitCompassHeading || Math.abs(e.alpha - 360);
+      setTransform(`translate(-50%, -50%) rotate(${-compass}deg)`)
+
+      // ±15 degree
+      if (
+        (pointDegree < Math.abs(compass) && pointDegree + 15 > Math.abs(compass)) ||
+        pointDegree > Math.abs(compass + 15) ||
+        pointDegree < Math.abs(compass)
+      ) {
+        setOpacity(0);
+      } else if (pointDegree) {
+        setOpacity(1);
+      }
     }
 
     function setTransform(data) {
        document.documentElement.style.setProperty('--transform-compass',data)
+    }
+
+    function setOpacity(data) {
+      document.documentElement.style.setProperty('--opacity-mypoint',data)
     }
 
     return (
@@ -38,7 +88,7 @@ function Compass(){
           <div className="compass-circle"></div>
           <div className="my-point"></div>
         </div>
-        <button className="start-btn" onClick={requestPer}>Start compass</button>
+        <button className="start-btn" onClick={init}>Start compass</button>
       </div>
     )
 }
